6972777e1fde56e7808b2d8d071a0aee43164502
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-17 09:20:40 -0400

    first commit

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -4,4 +4,12 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    Node head = null;
+
+    public class Node{
+        int element;
+        Node nextElement;
+        Node(int d){ element = d;}
+    }
 }

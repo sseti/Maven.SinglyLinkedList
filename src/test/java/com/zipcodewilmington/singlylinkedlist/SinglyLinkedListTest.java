7b258df8b82bb60e2c7b1d81b95fd8625fa9f9d4
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-18 07:46:47 -0400

    another commit

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -12,4 +12,26 @@ public class SinglyLinkedListTest {
         Node nextElement;
         Node(int d){ element = d;}
     }
-}
+
+    public void add(int data) {
+        Node current = head;
+        if(current == null){
+            head = new Node(data);
+        }
+        else {
+            while(current.nextElement != null){
+                current = current.nextElement;
+            }
+            current.nextElement = new Node(data);
+        }
+        public void remove(int data) {
+            Node current = head;
+            if (head.element == data) {
+                head = head.nextElement;
+            }
+            while (current.nextElement != null) {
+                if (current.nextElement.element == data) { current.nextElement = current.nextElement.nextElement; }
+                current = current.nextElement;
+            }
+    }
+

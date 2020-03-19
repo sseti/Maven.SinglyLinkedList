b5f254f4d3c3165ea7bf9c133edf6a0814b9f84e
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-19 09:46:16 -0400

    completed

--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,37 +1,88 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList singly;
 
-    Node head = null;
-
-    public class Node{
-        int element;
-        Node nextElement;
-        Node(int d){ element = d;}
-    }
-
-    public void add(int data) {
-        Node current = head;
-        if(current == null){
-            head = new Node(data);
+        @Before
+        public void init() {
+            singly = new SinglyLinkedList();
+            singly.add(1);
+            singly.add(5);
+            singly.add(2);
+            singly.add(6);
+            singly.add(3);
+            singly.add(4);
         }
-        else {
-            while(current.nextElement != null){
-                current = current.nextElement;
-            }
-            current.nextElement = new Node(data);
-        }
-        public void remove(int data) {
-            Node current = head;
-            if (head.element == data) {
-                head = head.nextElement;
-            }
-            while (current.nextElement != null) {
-                if (current.nextElement.element == data) { current.nextElement = current.nextElement.nextElement; }
-                current = current.nextElement;
-            }
-    }
 
+        @Test
+        public void TestAdd() {
+            singly.add(456);
+            Assert.assertTrue(singly.contains(456));
+        }
+
+        @Test
+        public void TestRemove() {
+            singly.remove(2);
+            Assert.assertFalse(singly.contains(2));
+        }
+
+        @Test
+        public void TestContains() {
+            Assert.assertFalse(singly.contains(10));
+            Assert.assertTrue(singly.contains(5));
+        }
+
+        @Test
+        public void TestFind() {
+            Assert.assertEquals(4, singly.find(3));
+            Assert.assertEquals(1, singly.find(5));
+            Assert.assertEquals(0, singly.find(1));
+        }
+
+        @Test
+        public void TestSize() {
+            Assert.assertEquals(6, singly.size());
+        }
+
+        @Test
+        public void TestSize2() {
+            singly.add(7);
+            singly.add(10);
+            singly.add(22);
+            singly.add(8);
+            Assert.assertEquals(10, singly.size());
+        }
+
+        @Test
+        public void TestGet() {
+            Assert.assertEquals(2, singly.get(2));
+            Assert.assertEquals(3, singly.get(4));
+        }
+
+        @Test
+        public void TestCopy() {
+            SinglyLinkedList copyList = new SinglyLinkedList();
+            copyList.add(1);
+            copyList.add(6);
+            copyList.add(2);
+            copyList.add(5);
+            copyList.add(3);
+            copyList.add(4);
+            Assert.assertEquals(5, copyList.copy().get(3));
+        }
+
+        @Test
+        public void TestSort() {
+            singly.sort();
+            Assert.assertEquals(3, singly.get(2));
+            Assert.assertEquals(5, singly.get(4));
+            Assert.assertEquals(6, singly.get(5));
+        }
+    }
